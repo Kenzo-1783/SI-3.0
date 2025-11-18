4019296618396730af059db8da8bc984a99ba2b9
JohanKael <118510163+JohanKael@users.noreply.github.com>
2025-11-18 20:32:34 +0300

    theme added

--- application/views/home.php
+++ application/views/home.php
@@ -3,9 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo base_url("../assets/css/global.css") ?>">
     <title>Home</title>
 </head>
 <body>
-    <p>Hello World</p>
+
+    <div class="nav">
+        <div class="nav-left">
+
+        </div>
+        <div class="nav-right">
+            
+        </div>
+    </div>
+
 </body>
 </html>
